0b8bdac0e1dd70496f669da02d06015858b7cec9
Yoma <codeyoma@gmail.com>
2022-07-20 02:17:08 +0900

    edit parse in map-part

--- include/get_map.h
+++ include/get_map.h
@@ -6,7 +6,7 @@
 /*   By: yongmkim <yongmkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 01:13:16 by yongmkim          #+#    #+#             */
-/*   Updated: 2022/07/20 01:40:31 by yongmkim         ###   ########seoul.kr  */
+/*   Updated: 2022/07/20 02:07:35 by yongmkim         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,34 @@
 
 enum e_map_check
 {
-	EMPTY = 1 << 0,
-	SPACE = 1 << 1,
-	WALL = 1 << 2,
-	POS_N = 1 << 3,
-	POS_S = 1 << 4,
-	POS_E = 1 << 5,
-	POS_W = 1 << 6,
+	SPACE = 1 << 0,
+	WALL = 1 << 1,
+	POS_N = 1 << 2,
+	POS_S = 1 << 3,
+	POS_E = 1 << 4,
+	POS_W = 1 << 5,
+	EMPTY = 1 << 8,
 	PLAYER = POS_N | POS_S | POS_E | POS_W
 };
 
 typedef struct s_dot
 {
+	int		type;
 	size_t	x;
 	size_t	y;
-	int		type;
 }			t_dot;
+
+typedef struct s_map_info
+{
+	t_dot	**map;
+	char	**temp_map;
+	char	*temp
+	size_t	str_vec_size;
+	size_t	cur;
+	int		gnl_check;
+	int		player_check;
+	size_t	width;
+	size_t	height;
+}			t_map_info;
 
 #endif
--- srcs/get_map.c
+++ srcs/get_map.c
@@ -6,13 +6,69 @@
 /*   By: yongmkim <yongmkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 01:12:28 by yongmkim          #+#    #+#             */
-/*   Updated: 2022/07/20 01:39:56 by yongmkim         ###   ########seoul.kr  */
+/*   Updated: 2022/07/20 02:16:51 by yongmkim         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_map.h"
 
+static void	_init_info(t_map_info *info)
+{
+	info->map = NULL;
+	info->temp_map = NULL;
+	info->temp = NULL;
+	info->str_vec_size = 2;
+	info->cur = 0;
+	info->gnl_check = -1;
+	info->player_check = 0;
+	info->height = 0;
+	info->width = 0;
+}
+
+static int	_append_map(t_map_info *info)
+{
+	char	**temp;
+
+	if (info->cur < 0)
+	{
+		temp = info->temp_map;
+		while (info->temp_map && *temp)
+			free(*(temp++));
+		free(info->temp_map);
+		return (-1);
+	}
+	if (info->cur < info->str_vec_size - 1)
+	{
+		info->temp_map[info->cur] = info->temp;
+		return (0);
+	}
+	info->str_vec_size <<= 1;
+	temp = ft_calloc(info->str_vec_size, sizeof(char *));
+	if (temp)
+	{
+		ft_memcpy(temp, info->temp_map, info->cur * (sizeof(char *)));
+		free(info->temp_map);
+		return (_append_map(info));
+	}
+	return (-1);
+}
+
 t_dot	**get_map(int fd)
 {
+	t_map_info	info;
 
+	info.gnl_check = get_next_line(fd, &info.temp);
+	while (info.gnl_check > 0)
+	{
+		if (_append_map(&info))
+			return (NULL);
+		info.temp = NULL;
+		info.gnl_check = get_next_line(fd, &info.temp);
+	}
+	if (info.gnl_check == -1)
+		return (NULL);
+	return (info.map);
 }
+
+// str to s_dot
+// check valid map
